4f6462470cb947c05517f38df6c4b728192ee95c
Stas Demchuk <standemchuk@gmail.com>
2018-07-14 12:53:11 +0300

    linting and testing utilities added

--- server.js
+++ server.js
@@ -9,35 +9,35 @@ const Pack = require('./package');
 const Routes = require('./routes');
 
 const init = async () => {
-    const server = await new Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  const server = await new Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  });
 
-    const swaggerOptions = {
-        info: {
-            title: 'Tiniest Blockchain API Documentation',
-            version: Pack.version
-        }
-    };
-
-    await server.register([
-        Inert,
-        Vision,
-        {
-            plugin: HapiSwagger,
-            options: swaggerOptions
-        }
-    ]);
-
-    try {
-        await server.start();
-        console.log(`Server running at: ${server.info.uri}`);
-    } catch(err) {
-        console.log(err)
+  const swaggerOptions = {
+    info: {
+      title: 'Tiniest Blockchain API Documentation',
+      version: Pack.version
     }
+  };
 
-    server.route(Routes);
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions
+    }
+  ]);
+
+  try {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+  } catch (err) {
+    console.log(err)
+  }
+
+  server.route(Routes);
 };
 
 init();
