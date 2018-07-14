e136d59cf31aca4274062c6620f2d70e6ad50d8b
Stas Demchuk <standemchuk@gmail.com>
2018-07-14 12:24:57 +0300

    swagger support, transaction endpoint boilerplate for swagger testing

---
+++ routes.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+module.exports = [
+    {
+        method: 'POST',
+        path: '/transaction',
+        config: {
+            handler: (request) => { return 'OK' },
+            description: 'Submit a new transaction to the transaction queue',
+            tags: ['api'],
+            validate: {
+                payload: Joi.object({
+                    from: Joi.string().required().description('Transaction sender id'),
+                    to: Joi.string().required().description('Transaction receiver id'),
+                    amount: Joi.number().required().description('Amount of coins sent in the' +
+                        ' transaction')
+                })
+            }
+        }
+    }
+];
--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
 
-const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
-});
+const Routes = require('./routes');
 
 const init = async () => {
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+    const server = await new Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    const swaggerOptions = {
+        info: {
+            title: 'Tiniest Blockchain API Documentation',
+            version: Pack.version
+        }
+    };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    try {
+        await server.start();
+        console.log(`Server running at: ${server.info.uri}`);
+    } catch(err) {
+        console.log(err)
+    }
+
+    server.route(Routes);
 };
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
 
 init();
